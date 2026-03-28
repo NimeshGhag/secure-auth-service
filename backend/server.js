eb1889f0b62d3f8b085548ae7cc5cfc526c32470
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-28 12:15:19 +0530

    feat: setup database connection and environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const connectToDB = require("./src/config/db");
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log("Server is Running on Port");
-});
+const startServer = async () => {
+  try {
+    await connectToDB();
+    app.listen(port, () => {
+      console.log(`Server is Running on Port ${port}`);
+    });
+  } catch (error) {
+    process.exit(1);
+    console.error("Error starting server:", error);
+  }
+};
+
+startServer();
