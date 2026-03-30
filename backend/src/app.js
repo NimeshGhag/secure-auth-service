e6b6ede29c22005b7810ca212df52283f32f9773
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-30 13:45:58 +0530

    feat: implement login api with validation, JWT, and cookie-based auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cokieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/auth.routes")
 
 const app = express();
 
 app.use(express.json());
+app.use(cokieParser());
 
 
 app.use("/api/auth", authRoutes);
