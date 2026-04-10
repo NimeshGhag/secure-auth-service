f2a22531393aa643dbca044a90be28122dee898b
NimeshGhag <ghagnimesh490@gmail.com>
2026-04-10 14:55:51 +0530

    feat: implement initial axios setup and API integration

---
+++ frontend/src/api/axios.config.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
+
+const instance = axios.create({
+  baseURL: `${API_BASE_URL}/api`,
+  withCredentials: true,
+});
+
+export default instance;
