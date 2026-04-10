62983db005db99c2e7831907e3d93d05079c8378
NimeshGhag <ghagnimesh490@gmail.com>
2026-04-10 11:44:29 +0530

    feat: implement helmet and cors for improved API security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cokieParser = require("cookie-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes")
 
 const app = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cokieParser());
