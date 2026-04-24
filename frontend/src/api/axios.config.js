bea4e4dedccc2635ed01ef56afd00073963ddee5
NimeshGhag <ghagnimesh490@gmail.com>
2026-04-24 19:53:22 +0530

    refactor: add mongoDB production uri

--- frontend/src/api/axios.config.js
+++ frontend/src/api/axios.config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
 const instance = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: `/api`,
   withCredentials: true,
 });
 
