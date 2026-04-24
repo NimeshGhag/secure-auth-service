d51dd032ca47ddf46da1cf6766a778487d1bc04e
NimeshGhag <ghagnimesh490@gmail.com>
2026-04-24 19:21:10 +0530

    feat: configure express to serve frontend for render deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,29 @@ const express = require("express");
 const cokieParser = require("cookie-parser");
 const helmet = require("helmet");
 const cors = require("cors");
+const path = require("path");
 
-const authRoutes = require("./routes/auth.routes")
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(helmet());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(cokieParser());
 
-
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get((req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 module.exports = app;
